8ef61289a2b2beaa6895429ac788f1f13e197ebd
CvODnE <nafilmnytfj@gmail.com>
2025-11-18 09:45:12 +0530

    Upadetd Footer

--- dishamedia/web/urls.py
+++ dishamedia/web/urls.py
@@ -1,5 +1,4 @@
 from django.urls import path
-from django.contrib.auth.decorators import login_required
 from . import views
 
 app_name = 'web'
@@ -11,9 +10,9 @@ urlpatterns = [
     path('news/<int:pk>/', views.news_detail, name='news_detail'),
     path('contact/', views.contact, name='contact'),
     path('verify-secret-code/', views.verify_secret_code, name='verify_secret_code'),
-    path('add/', login_required(views.add_content), name='add_content'),
-    path('add/news/', login_required(views.add_news), name='add_news'),
-    path('add/gallery/', login_required(views.add_gallery), name='add_gallery'),
+    path('add/', views.add_content, name='add_content'),
+    path('add/news/', views.add_news, name='add_news'),
+    path('add/gallery/', views.add_gallery, name='add_gallery'),
     path('delete-news/<int:pk>/', views.delete_news, name='delete_news'),
     path('delete-gallery/<int:pk>/', views.delete_gallery, name='delete_gallery'),
 ]
